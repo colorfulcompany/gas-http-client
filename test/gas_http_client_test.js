681fb4ae23b26a4c718b1ac9ebc4e97b7098a4b7
T.Watanabe <watanabe@colorfulcompany.co.jp>
2017-07-20 15:57:41 +0900

    app() と isValidMethod() を追加

--- test/gas_http_client_test.js
+++ test/gas_http_client_test.js
@@ -1,7 +1,38 @@
 import GasHttpClient from '../lib/gas_http_client'
+import assert        from 'power-assert'
 
 describe('GasHttpClient', ()=> {
-  it('', ()=> {
-    console.log(new GasHttpClient())
+  let client :string
+  
+  beforeEach(()=> {
+    client = new GasHttpClient({}, 'http://localhost:3000')
+  })
+
+  describe('#app', ()=> {
+    it('return object received with first argument', ()=> {
+      assert.deepEqual({}, client.app())
+    })
+  })
+
+  describe('#methods', ()=> {
+    it('', ()=> {
+      assert.deepEqual(
+        ['get', 'delete', 'patch', 'post', 'put'],
+        client.methods())
+    })
+  })
+
+  describe('#isValidMethod', ()=> {
+    it('put is valid', ()=> {
+      assert.equal(true, client.isValidMethod('put'))
+    })
+
+    it('option is invalid', ()=> {
+      assert.equal(false, client.isValidMethod('option'))
+    })
+
+    it('1 is invalid', ()=> {
+      assert.equal(false, client.isValidMethod(1))
+    })
   })
 })
